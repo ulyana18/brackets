cfb7f2a09bf157f6d8f4f8aa56ed48951c7d3ada
Ulyana Bychkovskaya <ubychkovskaya20@gmail.com>
2019-10-01 13:51:17 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let firstbrackets='';
+  for(let i=0; i<str.length; i++) {
+    for(let n=0; n<bracketsConfig.length; n++) {
+      if(str[i]===bracketsConfig[n][0] && bracketsConfig[n][0]===bracketsConfig[n][1]) {
+        if(firstbrackets[firstbrackets.length-1]===bracketsConfig[n][0]) {
+          firstbrackets=firstbrackets.slice(0, firstbrackets.length-1);
+          break;
+        }
+        if(firstbrackets[firstbrackets.length-1]!==bracketsConfig[n][0]) {
+          firstbrackets+=str[i]; 
+          break;
+        }
+        return false;
+      }
+
+      if(str[i]===bracketsConfig[n][0]) {
+        firstbrackets+=str[i]; 
+        break;
+      }
+      if(str[i]===bracketsConfig[n][1]) {
+        if(firstbrackets[firstbrackets.length-1]===bracketsConfig[n][0]) {
+          firstbrackets=firstbrackets.slice(0, firstbrackets.length-1);
+          break;
+        }
+        else return false;
+      }
+    }
+  }
+  if(firstbrackets === '')
+  return true;
+  else return false;
 }
